71b18674afe3f1432ba11d4e01bfd808c2a5df1c
Katherine Deegan <katherine.deegan@utexas.edu>
2023-09-06 17:59:59 +0000

    adding remaining files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    # when visiting URL/static_pages/home, code in here is executed
+    # then VIEW is rendered corresponding to this action
   end
 
   def help
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # maps GET http request URL/static_pages/home to home action in Static Pages controller
   get 'static_pages/home'
+
   get 'static_pages/help'
+
+  get "static_pages/about" # adding this line of code automatically created static_pages_about_url helper
+
   root "application#hello"
 end
